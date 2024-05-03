cd798aad4949edaded841e10e0fc8eda7b1c47b3
Alessandro-cesf <116165893+kouoji@users.noreply.github.com>
2024-05-03 11:23:15 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
 import os
 
-#restaurantes = ["Pé de banha", "Coritiba feijoadas"]
-#Inserir dicionario em outra linguagem, chave valor
-restaurantes=[{'nome':'Bife sujo','categoria':'prato-feito','ativo':True},
-             {'nome':'Saco de feijao','categoria':'feijoada','ativo':False},
-             {'nome':'Pé de banha','categoria':'pastelaria','ativo':True}]
+restaurantes = [
+    {'nome': 'Bife sujo', 'categoria': 'prato-feito', 'ativo': True},
+    {'nome': 'Saco de feijao', 'categoria': 'feijoada', 'ativo': False},
+    {'nome': 'Pé de banha', 'categoria': 'pastelaria', 'ativo': True}
+]
 
 def finalizar_app():
     os.system("clear")
     os.system("cls")
     print("Finalizando o app\n")
-   
+
 def voltar_menu_principal():
     input("Digite uma tecla para voltar ao menu principal: ")
-    main()
 
 def mostrar_subtitulo(texto):
     os.system("clear")
     print(texto)
     print()
-   
-
 
 def escolher_opcoes():
     mostrar_subtitulo("Programa Expresso\n")
@@ -34,52 +31,50 @@ def opcao_invalida():
     voltar_menu_principal()
 
 def chamar_nome_do_app():
-    print("'ℝ𝕖𝕤𝕥𝕒𝕦𝕣𝕒𝕟𝕥𝕖 𝕖𝕩𝕡𝕣𝕖𝕤𝕤𝕠'")
+    print("'ℝ𝕖𝕠𝕦𝕣𝕠𝕟𝕠𝕥𝕖 𝕖𝕩𝕡𝕣𝕖𝕠𝕠'")
 
 def listarRestaurantes():
-   
     mostrar_subtitulo('Listando os Restaurantes')
     for restaurante in restaurantes:
-       
-
-        #modificar a maneira de listar para o dicionario
-        nome_restaurante=restaurantes['nome']
-        categoria=restaurante['categoria']
-        print(f'-{nome_restaurante}--{categoria}',--{ative}')
-      ativo=restaurante['ativi']
-      voltar_menu_principal()
-       
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = restaurante['ativo']
+        print(f'-{nome_restaurante}--{categoria}--{ativo}')
 
 def cadastrar_novo_restaurante():
     nome_do_restaurante = input("Digite o nome do novo restaurante: ")
-    categoria =input ( 'Digite a categoria do restaurante{nome_do_restaurante}')
-    Dados_do_restaurante= {nome: nome_do_restaurante, 'categoria', categoria, 'ative': false}
-    restaurantes.apend(dados_do_restaurante)
-    restaurantes.append(nome_do_restaurante)
+    categoria = input(f'Digite a categoria do restaurante{nome_do_restaurante}:')
+    dados_do_restaurante = {'nome': nome_do_restaurante, 'categoria': categoria, 'ativo':False}
+    restaurantes.append(dados_do_restaurante)
     print(f"Você cadastrou o restaurante: {nome_do_restaurante}")
-    voltar_menu_principal()
-   
 
 def main():
-    escolher_opcoes()
-    chamar_nome_do_app()
-    try:
-        opcaodigitada = int(input("Digite a opção desejada: "))
-        if opcaodigitada == 1:
-            print("Você escolheu cadastrar restaurante\n")
-            cadastrar_novo_restaurante()
-        elif opcaodigitada == 2:
-            listarRestaurantes()
-        elif opcaodigitada == 3:
-            print("Você escolheu ativar restaurante\n")
-        elif opcaodigitada == 4:
-            print("Você escolheu sair do aplicativo\n")
-            finalizar_app()
-        else:
-            opcao_invalida()
-    except ValueError:
-        opcao_invalida()
+    while True:
+        try:
+            escolher_opcoes()
+            opcaodigitada = int(input("Digite a opção desejada: "))
+            if opcaodigitada == 1:
+                print("Você escolheu cadastrar restaurante\n")
+                cadastrar_novo_restaurante()
+                main()
+            elif opcaodigitada == 2:
+                listarRestaurantes()
+                voltar_menu_principal()
+                main()
+            elif opcaodigitada == 3:
+                print("Você escolheu ativar restaurante\n")
+                main()
+            elif opcaodigitada == 4:
+                print("Você escolheu sair do aplicativo\n")
+                finalizar_app()
+                break
+            else:
+                opcao_invalida()
+                main()
+        except ValueError:
+            print("Por favor, digite um número.")
+            main()
 
 if __name__ == "__main__":
-    finalizar_app()
+    chamar_nome_do_app()
     main()
